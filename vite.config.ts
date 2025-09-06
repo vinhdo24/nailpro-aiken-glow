959b5c481423c22af833bd179114c5c6312d2285
vinhdo24 <seoggreview@gmail.com>
2025-09-06 07:26:00 -0700

    Set base path to '/' for custom domain root

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/nailpro-aiken-glow/',
+  base: mode === 'development' ? '/' : '/'
   server: {
     host: "::",
     port: 8080,
